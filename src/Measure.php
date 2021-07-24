3fa72b319c68d24b3fbaa0e436f416c3810c5200
Christiaan Bye <christiaanbye@gmail.com>
2021-07-24 16:17:07 +0200

    Added split functionality to the stopwatch

--- src/Measure.php
+++ src/Measure.php
@@ -3,6 +3,7 @@
 namespace Measure;
 
 use function is_int;
+use function is_string;
 use function microtime;
 
 class Measure
@@ -13,9 +14,29 @@ class Measure
     /** @var float|int */
     private static $stopTime;
 
+    private static $splits = array();
+
     public static function start()
     {
         self::$startTime = self::getTime();
+    }
+
+    /**
+     * @param string $key A name to identify the intermediate time amongst the results
+     *
+     * @throws \InvalidArgumentException If an empty value or a non-string value was passed as key
+     */
+    public static function split($key)
+    {
+        if (!isset(self::$startTime)) {
+            throw new \RuntimeException('The intermediate time cannot be recorded as the stopwatch has not been started.');
+        }
+
+        if (empty($key) || !is_string($key)) {
+            throw new \InvalidArgumentException('Please enter a valid string as key to identify the intermediate time.');
+        }
+
+        self::$splits[$key] = self::getTime();
     }
 
     public static function stop()
@@ -24,16 +45,51 @@ class Measure
     }
 
     /**
-     * @return float
+     * @return float Either the amount of time passed since the stopwatch was started or the time elapsed between
+     *               starting and stopping the stopwatch
      */
-    public static function getResult()
+    public static function elapsed()
     {
-        $result = self::$stopTime - self::$startTime;
-
-        if (is_int($result)) {
-            // Convert nanoseconds to seconds
-            $result /= 1e9;
+        if (!isset(self::$startTime)) {
+            throw new \RuntimeException('The elapsed time can not be reported as the stopwatch has never been started.');
         }
+
+        if (isset(self::$stopTime)) {
+            $currentTime = self::$stopTime;
+        } else {
+            $currentTime = self::getTime();
+        }
+
+        return self::diff(self::$startTime, $currentTime);
+    }
+
+    /**
+     * @return array An array containing all the recorded intermediate times and the time difference between them
+     */
+    public static function getSplits()
+    {
+        // Stop the time if it was left running
+        if (isset(self::$stopTime)) {
+            self::stop();
+        }
+
+        $result        = array();
+        $previousSplit = 0;
+
+        foreach (self::$splits as $key => $split) {
+            $splitResult['sinceStart'] = self::diff(self::$startTime, $split);
+
+            if ($previousSplit !== 0) {
+                $splitResult['sincePreviousSplit'] = self::diff($previousSplit, $split);
+            }
+
+            $result[$key]  = $splitResult;
+            $previousSplit = $split;
+        }
+
+        $result['overall'] = array(
+            'sinceStart' => self::diff(self::$startTime, self::$stopTime)
+        );
 
         return $result;
     }
@@ -45,5 +101,17 @@ class Measure
         }
 
         return microtime(true);
+    }
+
+    private static function diff($firstTime, $secondTime)
+    {
+        $result = $secondTime - $firstTime;
+
+        if (is_int($result)) {
+            // Convert nanoseconds to seconds
+            $result /= 1e9;
+        }
+
+        return $result;
     }
 }
--- test.php
+++ test.php
@@ -6,6 +6,15 @@ require_once 'vendor/autoload.php';
 
 Measure::start();
 usleep(1000);
+var_dump(Measure::elapsed());
+Measure::split('Split 1');
+var_dump(Measure::elapsed());
+usleep(1200);
+Measure::split('Split 2');
+usleep(1400);
 Measure::stop();
+var_dump(Measure::elapsed());
+var_dump(Measure::elapsed());
+var_dump(Measure::elapsed());
 
-var_dump(Measure::getResult());
+var_dump(Measure::getSplits());
